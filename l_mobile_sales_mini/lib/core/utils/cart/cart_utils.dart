1765d07717e03b75d6770c23072d7da41a62c83b
Morris K.N <momalley042@gmail.com>
2025-12-29 16:49:04 +0300

    do order id generate

--- l_mobile_sales_mini/lib/core/utils/cart/cart_utils.dart
+++ l_mobile_sales_mini/lib/core/utils/cart/cart_utils.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:l_mobile_sales_mini/data/models/products/product_model.dart';
 import 'package:l_mobile_sales_mini/presentation/widgets/common/dialogs/select_customer_dialog_widget.dart';
@@ -19,4 +21,16 @@ Future<Product?> getProductFromDialog() async {
   );
 
   return selected;
+}
+
+String generateOrderId() {
+  final random = Random();
+  final timePart = DateTime.now().millisecondsSinceEpoch.toRadixString(36);
+  final randomPart = random
+      .nextInt(36 * 36 * 36)
+      .toRadixString(36)
+      .padLeft(3, '0');
+
+  final id = (timePart + randomPart).toUpperCase();
+  return id.length > 7 ? id.substring(id.length - 7) : id;
 }
